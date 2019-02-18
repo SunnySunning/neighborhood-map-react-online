dd58951202ac42127af5a8573c10874973671882
sunny <sunny.sun@yingzt.com>
2019-02-18 17:02:30 +0800

    commit

---
+++ precache-manifest.591e8e1ccf35c20d9475e2eda345c8a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b0b8751edbbb9c59bb7",
+    "url": "/static/css/main.0d444daa.chunk.css"
+  },
+  {
+    "revision": "9b0b8751edbbb9c59bb7",
+    "url": "/static/js/main.9b0b8751.chunk.js"
+  },
+  {
+    "revision": "7cc64ff5897ad3d8c99f",
+    "url": "/static/js/1.7cc64ff5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8dad35cf34fd44323daedeb9eb2ef1be",
+    "url": "/index.html"
+  }
+];
